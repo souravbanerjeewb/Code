bd1bcdc06a4b1a03c067cf34aeb6ae5000dc8732
Sourav Banerjee <sourav.banerjee.wb@gmail.com>
2020-10-27 22:55:15 +0530

    List to string
    Convert any List to String

---
+++ List_to_String.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def listToString(s):  
+    
+    # initialize an empty string 
+    str1 = ""  
+    
+    # traverse in the string   
+    for ele in s:  
+        str1 += ele   
+    
+    # return string   
+    return str1  
+        
+        
+# Driver code     
+s = ['Sky', 'is', 'Blue'] 
+print(listToString(s))  
+
+
+# In[ ]:
+
+
+
+
